2a05290977d5d7046d6913a583937159b6ffb50d
sky-adams <skyadams@sbunified.org>
2018-02-09 08:27:20 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -41,7 +41,7 @@ def home():
 
 @app.route('/login')
 def login():   
-    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
+    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https'))
 
 @app.route('/logout')
 def logout():
