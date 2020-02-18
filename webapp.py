a8c0071a15f2035a61ead46b4f0c7accf0dfe0f8
sky-adams <skyadams@sbunified.org>
2020-02-18 09:56:58 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,7 +17,7 @@ app.debug = True #Change this to False for production
 app.secret_key = os.environ['SECRET_KEY'] 
 oauth = OAuth(app)
 
-
+#Set up GitHub as the OAuth provider
 github = oauth.remote_app(
     'github',
     consumer_key=os.environ['GITHUB_CLIENT_ID'], 
@@ -48,7 +48,7 @@ def logout():
     session.clear()
     return render_template('message.html', message='You were logged out')
 
-@app.route()#the route should match the callback URL registered with the OAuth provider
+@app.route('/login/authorized')#the route should match the callback URL registered with the OAuth provider
 def authorized():
     resp = github.authorized_response()
     if resp is None:
@@ -57,8 +57,14 @@ def authorized():
     else:
         try:
             #save user data and set log in message
+            session['github_token'] = (resp['access_token'], '')
+            session['user_data'] = github.get('user').data
+            message = 'You were successfully logged in as ' + session['user_data']['login'] + '.'
         except Exception as inst:
             #clear the session and give error message
+            session.clear()
+            print(inst)
+            message = 'Unable to login.  Please try again.'
     return render_template('message.html', message=message)
 
 
